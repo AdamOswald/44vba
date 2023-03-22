3eee2b2502e7dd70f08fa2460f372006e3179614
Adam Oswald <adamoswald69420@gmail.com>
2023-03-22 09:10:23 -0400

    Update compat_posix_string.c

--- libretro-common/compat/compat_posix_string.c
+++ libretro-common/compat/compat_posix_string.c
@@ -1,7 +1,7 @@
-/* Copyright  (C) 2010-2015 The RetroArch team
+/* Copyright  (C) 2010-2020 The RetroArch team
  *
  * ---------------------------------------------------------------------------------------
- * The following license statement only applies to this file (compat.c).
+ * The following license statement only applies to these files (compat_posix_string.c) and (compat.c).
  * ---------------------------------------------------------------------------------------
  *
  * Permission is hereby granted, free of charge,
