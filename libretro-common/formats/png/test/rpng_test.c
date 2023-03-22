84af761f97ad76f780209a2710abed3edd5e7e54
Restyled.io <commits@restyled.io>
2023-03-22 13:54:42 +0000

    Restyled by clang-format

--- libretro-common/formats/png/test/rpng_test.c
+++ libretro-common/formats/png/test/rpng_test.c
@@ -38,96 +38,96 @@
 
 static bool rpng_load_image_argb(const char *path, uint32_t **data,
                                  unsigned *width, unsigned *height) {
-    int retval;
-    size_t file_len;
-    bool ret = true;
-    rpng_t *rpng = NULL;
-    void *ptr = NULL;
-    struct nbio_t *handle = (struct nbio_t *)nbio_open(path, NBIO_READ);
+  int retval;
+  size_t file_len;
+  bool ret = true;
+  rpng_t *rpng = NULL;
+  void *ptr = NULL;
+  struct nbio_t *handle = (struct nbio_t *)nbio_open(path, NBIO_READ);
 
-    if (!handle)
-        goto end;
+  if (!handle)
+    goto end;
 
-    nbio_begin_read(handle);
+  nbio_begin_read(handle);
 
-    while (!nbio_iterate(handle))
-        ;
+  while (!nbio_iterate(handle))
+    ;
 
-    ptr = nbio_get_ptr(handle, &file_len);
+  ptr = nbio_get_ptr(handle, &file_len);
 
-    if (!ptr) {
-        ret = false;
-        goto end;
-    }
+  if (!ptr) {
+    ret = false;
+    goto end;
+  }
 
-    rpng = rpng_alloc();
+  rpng = rpng_alloc();
 
-    if (!rpng) {
-        ret = false;
-        goto end;
-    }
+  if (!rpng) {
+    ret = false;
+    goto end;
+  }
 
-    if (!rpng_set_buf_ptr(rpng, (uint8_t *)ptr)) {
-        ret = false;
-        goto end;
-    }
+  if (!rpng_set_buf_ptr(rpng, (uint8_t *)ptr)) {
+    ret = false;
+    goto end;
+  }
 
-    if (!rpng_start(rpng)) {
-        ret = false;
-        goto end;
-    }
+  if (!rpng_start(rpng)) {
+    ret = false;
+    goto end;
+  }
 
-    while (rpng_iterate_image(rpng))
-        ;
+  while (rpng_iterate_image(rpng))
+    ;
 
-    if (!rpng_is_valid(rpng)) {
-        ret = false;
-        goto end;
-    }
+  if (!rpng_is_valid(rpng)) {
+    ret = false;
+    goto end;
+  }
 
-    do {
-        retval = rpng_process_image(rpng, (void **)data, file_len, width, height);
-    } while (retval == IMAGE_PROCESS_NEXT);
+  do {
+    retval = rpng_process_image(rpng, (void **)data, file_len, width, height);
+  } while (retval == IMAGE_PROCESS_NEXT);
 
-    if (retval == IMAGE_PROCESS_ERROR || retval == IMAGE_PROCESS_ERROR_END)
-        ret = false;
+  if (retval == IMAGE_PROCESS_ERROR || retval == IMAGE_PROCESS_ERROR_END)
+    ret = false;
 
 end:
-    if (handle)
-        nbio_free(handle);
-    if (rpng)
-        rpng_free(rpng);
-    rpng = NULL;
-    if (!ret)
-        free(*data);
-    return ret;
+  if (handle)
+    nbio_free(handle);
+  if (rpng)
+    rpng_free(rpng);
+  rpng = NULL;
+  if (!ret)
+    free(*data);
+  return ret;
 }
 
 static int test_rpng(const char *in_path) {
 #ifdef HAVE_IMLIB2
-    Imlib_Image img;
-    const uint32_t *imlib_data = NULL;
+  Imlib_Image img;
+  const uint32_t *imlib_data = NULL;
 #endif
-    const uint32_t test_data[] = {
-        0xff000000 | 0x50, 0xff000000 | 0x80, 0xff000000 | 0x40,
-        0xff000000 | 0x88, 0xff000000 | 0x50, 0xff000000 | 0x80,
-        0xff000000 | 0x40, 0xff000000 | 0x88, 0xff000000 | 0xc3,
-        0xff000000 | 0xd3, 0xff000000 | 0xc3, 0xff000000 | 0xd3,
-        0xff000000 | 0xc3, 0xff000000 | 0xd3, 0xff000000 | 0xc3,
-        0xff000000 | 0xd3,
-    };
-    uint32_t *data = NULL;
-    unsigned width = 0;
-    unsigned height = 0;
+  const uint32_t test_data[] = {
+      0xff000000 | 0x50, 0xff000000 | 0x80, 0xff000000 | 0x40,
+      0xff000000 | 0x88, 0xff000000 | 0x50, 0xff000000 | 0x80,
+      0xff000000 | 0x40, 0xff000000 | 0x88, 0xff000000 | 0xc3,
+      0xff000000 | 0xd3, 0xff000000 | 0xc3, 0xff000000 | 0xd3,
+      0xff000000 | 0xc3, 0xff000000 | 0xd3, 0xff000000 | 0xc3,
+      0xff000000 | 0xd3,
+  };
+  uint32_t *data = NULL;
+  unsigned width = 0;
+  unsigned height = 0;
 
-    if (!rpng_save_image_argb("/tmp/test.png", test_data, 4, 4, 16))
-        return 1;
+  if (!rpng_save_image_argb("/tmp/test.png", test_data, 4, 4, 16))
+    return 1;
 
-    if (!rpng_load_image_argb(in_path, &data, &width, &height))
-        return 2;
+  if (!rpng_load_image_argb(in_path, &data, &width, &height))
+    return 2;
 
-    fprintf(stderr, "Path: %s.\n", in_path);
-    fprintf(stderr, "Got image: %u x %u.\n", width, height);
+  fprintf(stderr, "Path: %s.\n", in_path);
+  fprintf(stderr, "Got image: %u x %u.\n", width, height);
 
 #if 0
     fprintf(stderr, "\nRPNG:\n");
@@ -141,16 +141,16 @@ static int test_rpng(const char *in_path) {
 #endif
 
 #ifdef HAVE_IMLIB2
-    /* Validate with imlib2 as well. */
-    img = imlib_load_image(in_path);
-    if (!img)
-        return 4;
+  /* Validate with imlib2 as well. */
+  img = imlib_load_image(in_path);
+  if (!img)
+    return 4;
 
-    imlib_context_set_image(img);
+  imlib_context_set_image(img);
 
-    width = imlib_image_get_width();
-    height = imlib_image_get_width();
-    imlib_data = imlib_image_get_data_for_reading_only();
+  width = imlib_image_get_width();
+  height = imlib_image_get_width();
+  imlib_data = imlib_image_get_data_for_reading_only();
 
 #if 0
     fprintf(stderr, "\nImlib:\n");
@@ -162,36 +162,36 @@ static int test_rpng(const char *in_path) {
     }
 #endif
 
-    if (memcmp(imlib_data, data, width * height * sizeof(uint32_t)) != 0) {
-        fprintf(stderr, "Imlib and RPNG differs!\n");
-        return 5;
-    } else
-        fprintf(stderr, "Imlib and RPNG are equivalent!\n");
+  if (memcmp(imlib_data, data, width * height * sizeof(uint32_t)) != 0) {
+    fprintf(stderr, "Imlib and RPNG differs!\n");
+    return 5;
+  } else
+    fprintf(stderr, "Imlib and RPNG are equivalent!\n");
 
-    imlib_free_image();
+  imlib_free_image();
 #endif
-    free(data);
+  free(data);
 
-    return 0;
+  return 0;
 }
 
 int main(int argc, char *argv[]) {
-    const char *in_path = "/tmp/test.png";
+  const char *in_path = "/tmp/test.png";
 
-    if (argc > 2) {
-        fprintf(stderr, "Usage: %s <png file>\n", argv[0]);
-        return 1;
-    }
+  if (argc > 2) {
+    fprintf(stderr, "Usage: %s <png file>\n", argv[0]);
+    return 1;
+  }
 
-    if (argc == 2)
-        in_path = argv[1];
+  if (argc == 2)
+    in_path = argv[1];
 
-    fprintf(stderr, "Doing tests...\n");
+  fprintf(stderr, "Doing tests...\n");
 
-    if (test_rpng(in_path) != 0) {
-        fprintf(stderr, "Test failed.\n");
-        return -1;
-    }
+  if (test_rpng(in_path) != 0) {
+    fprintf(stderr, "Test failed.\n");
+    return -1;
+  }
 
-    return 0;
+  return 0;
 }
