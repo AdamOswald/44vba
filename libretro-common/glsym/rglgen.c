09d30483eb1a3776a651e9f8adcefc7cde941902
Restyled.io <commits@restyled.io>
2023-03-22 13:47:30 +0000

    Restyled by astyle

--- libretro-common/glsym/rglgen.c
+++ libretro-common/glsym/rglgen.c
@@ -33,12 +33,12 @@
 
 void rglgen_resolve_symbols_custom(rglgen_proc_address_t proc,
                                    const struct rglgen_sym_map *map) {
-  for (; map->sym; map++) {
-    rglgen_func_t func = proc(map->sym);
-    memcpy(map->ptr, &func, sizeof(func));
-  }
+    for (; map->sym; map++) {
+        rglgen_func_t func = proc(map->sym);
+        memcpy(map->ptr, &func, sizeof(func));
+    }
 }
 
 void rglgen_resolve_symbols(rglgen_proc_address_t proc) {
-  rglgen_resolve_symbols_custom(proc, rglgen_symbol_map);
+    rglgen_resolve_symbols_custom(proc, rglgen_symbol_map);
 }
