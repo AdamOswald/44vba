da26b7084f9f6ce352f6c03259881b5984696274
Restyled.io <commits@restyled.io>
2023-03-22 13:51:43 +0000

    Restyled by clang-format

--- libretro-common/formats/image_transfer.c
+++ libretro-common/formats/image_transfer.c
@@ -44,219 +44,219 @@
 #include <formats/image.h>
 
 void image_transfer_free(void *data, enum image_type_enum type) {
-    switch (type) {
-    case IMAGE_TYPE_TGA:
+  switch (type) {
+  case IMAGE_TYPE_TGA:
 #ifdef HAVE_RTGA
-        rtga_free((rtga_t *)data);
+    rtga_free((rtga_t *)data);
 #endif
-        break;
-    case IMAGE_TYPE_PNG:
+    break;
+  case IMAGE_TYPE_PNG:
 #ifdef HAVE_RPNG
-        rpng_free((rpng_t *)data);
+    rpng_free((rpng_t *)data);
 #endif
-        break;
-    case IMAGE_TYPE_JPEG:
+    break;
+  case IMAGE_TYPE_JPEG:
 #ifdef HAVE_RJPEG
-        rjpeg_free((rjpeg_t *)data);
+    rjpeg_free((rjpeg_t *)data);
 #endif
-        break;
-    case IMAGE_TYPE_BMP:
+    break;
+  case IMAGE_TYPE_BMP:
 #ifdef HAVE_RBMP
-        rbmp_free((rbmp_t *)data);
+    rbmp_free((rbmp_t *)data);
 #endif
-        break;
-    case IMAGE_TYPE_NONE:
-        break;
-    }
+    break;
+  case IMAGE_TYPE_NONE:
+    break;
+  }
 }
 
 void *image_transfer_new(enum image_type_enum type) {
-    switch (type) {
-    case IMAGE_TYPE_PNG:
+  switch (type) {
+  case IMAGE_TYPE_PNG:
 #ifdef HAVE_RPNG
-        return rpng_alloc();
+    return rpng_alloc();
 #else
-        break;
+    break;
 #endif
-    case IMAGE_TYPE_JPEG:
+  case IMAGE_TYPE_JPEG:
 #ifdef HAVE_RJPEG
-        return rjpeg_alloc();
+    return rjpeg_alloc();
 #else
-        break;
+    break;
 #endif
-    case IMAGE_TYPE_TGA:
+  case IMAGE_TYPE_TGA:
 #ifdef HAVE_RTGA
-        return rtga_alloc();
+    return rtga_alloc();
 #else
-        break;
+    break;
 #endif
-    case IMAGE_TYPE_BMP:
+  case IMAGE_TYPE_BMP:
 #ifdef HAVE_RBMP
-        return rbmp_alloc();
+    return rbmp_alloc();
 #else
-        break;
+    break;
 #endif
-    default:
-        break;
-    }
+  default:
+    break;
+  }
 
-    return NULL;
+  return NULL;
 }
 
 bool image_transfer_start(void *data, enum image_type_enum type) {
 
-    switch (type) {
-    case IMAGE_TYPE_PNG:
+  switch (type) {
+  case IMAGE_TYPE_PNG:
 #ifdef HAVE_RPNG
-        if (!rpng_start((rpng_t *)data))
-            break;
-        return true;
+    if (!rpng_start((rpng_t *)data))
+      break;
+    return true;
 #else
-        break;
+    break;
 #endif
-    case IMAGE_TYPE_JPEG:
+  case IMAGE_TYPE_JPEG:
 #ifdef HAVE_RJPEG
-        return true;
+    return true;
 #else
-        break;
+    break;
 #endif
-    case IMAGE_TYPE_TGA:
+  case IMAGE_TYPE_TGA:
 #ifdef HAVE_RTGA
-        return true;
+    return true;
 #else
-        break;
+    break;
 #endif
-    case IMAGE_TYPE_BMP:
-        return true;
-    case IMAGE_TYPE_NONE:
-        break;
-    }
+  case IMAGE_TYPE_BMP:
+    return true;
+  case IMAGE_TYPE_NONE:
+    break;
+  }
 
-    return false;
+  return false;
 }
 
 bool image_transfer_is_valid(void *data, enum image_type_enum type) {
-    switch (type) {
-    case IMAGE_TYPE_PNG:
+  switch (type) {
+  case IMAGE_TYPE_PNG:
 #ifdef HAVE_RPNG
-        return rpng_is_valid((rpng_t *)data);
+    return rpng_is_valid((rpng_t *)data);
 #else
-        break;
+    break;
 #endif
-    case IMAGE_TYPE_JPEG:
+  case IMAGE_TYPE_JPEG:
 #ifdef HAVE_RJPEG
-        return true;
+    return true;
 #else
-        break;
+    break;
 #endif
-    case IMAGE_TYPE_TGA:
+  case IMAGE_TYPE_TGA:
 #ifdef HAVE_RTGA
-        return true;
+    return true;
 #else
-        break;
+    break;
 #endif
-    case IMAGE_TYPE_BMP:
-        return true;
-    case IMAGE_TYPE_NONE:
-        break;
-    }
+  case IMAGE_TYPE_BMP:
+    return true;
+  case IMAGE_TYPE_NONE:
+    break;
+  }
 
-    return false;
+  return false;
 }
 
 void image_transfer_set_buffer_ptr(void *data, enum image_type_enum type,
                                    void *ptr) {
-    switch (type) {
-    case IMAGE_TYPE_PNG:
+  switch (type) {
+  case IMAGE_TYPE_PNG:
 #ifdef HAVE_RPNG
-        rpng_set_buf_ptr((rpng_t *)data, (uint8_t *)ptr);
+    rpng_set_buf_ptr((rpng_t *)data, (uint8_t *)ptr);
 #endif
-        break;
-    case IMAGE_TYPE_JPEG:
+    break;
+  case IMAGE_TYPE_JPEG:
 #ifdef HAVE_RJPEG
-        rjpeg_set_buf_ptr((rjpeg_t *)data, (uint8_t *)ptr);
+    rjpeg_set_buf_ptr((rjpeg_t *)data, (uint8_t *)ptr);
 #endif
-        break;
-    case IMAGE_TYPE_TGA:
+    break;
+  case IMAGE_TYPE_TGA:
 #ifdef HAVE_RTGA
-        rtga_set_buf_ptr((rtga_t *)data, (uint8_t *)ptr);
+    rtga_set_buf_ptr((rtga_t *)data, (uint8_t *)ptr);
 #endif
-        break;
-    case IMAGE_TYPE_BMP:
+    break;
+  case IMAGE_TYPE_BMP:
 #ifdef HAVE_RBMP
-        rbmp_set_buf_ptr((rbmp_t *)data, (uint8_t *)ptr);
+    rbmp_set_buf_ptr((rbmp_t *)data, (uint8_t *)ptr);
 #endif
-        break;
-    case IMAGE_TYPE_NONE:
-        break;
-    }
+    break;
+  case IMAGE_TYPE_NONE:
+    break;
+  }
 }
 
 int image_transfer_process(void *data, enum image_type_enum type,
                            uint32_t **buf, size_t len, unsigned *width,
                            unsigned *height) {
-    switch (type) {
-    case IMAGE_TYPE_PNG:
+  switch (type) {
+  case IMAGE_TYPE_PNG:
 #ifdef HAVE_RPNG
-        if (!rpng_is_valid((rpng_t *)data))
-            return IMAGE_PROCESS_ERROR;
+    if (!rpng_is_valid((rpng_t *)data))
+      return IMAGE_PROCESS_ERROR;
 
-        return rpng_process_image((rpng_t *)data, (void **)buf, len, width, height);
+    return rpng_process_image((rpng_t *)data, (void **)buf, len, width, height);
 #else
-        break;
+    break;
 #endif
-    case IMAGE_TYPE_JPEG:
+  case IMAGE_TYPE_JPEG:
 #ifdef HAVE_RJPEG
-        return rjpeg_process_image((rjpeg_t *)data, (void **)buf, len, width,
-                                   height);
+    return rjpeg_process_image((rjpeg_t *)data, (void **)buf, len, width,
+                               height);
 #else
-        break;
+    break;
 #endif
-    case IMAGE_TYPE_TGA:
+  case IMAGE_TYPE_TGA:
 #ifdef HAVE_RTGA
-        return rtga_process_image((rtga_t *)data, (void **)buf, len, width, height);
+    return rtga_process_image((rtga_t *)data, (void **)buf, len, width, height);
 #else
-        break;
+    break;
 #endif
-    case IMAGE_TYPE_BMP:
+  case IMAGE_TYPE_BMP:
 #ifdef HAVE_RBMP
-        return rbmp_process_image((rbmp_t *)data, (void **)buf, len, width, height);
+    return rbmp_process_image((rbmp_t *)data, (void **)buf, len, width, height);
 #else
-        break;
+    break;
 #endif
-    case IMAGE_TYPE_NONE:
-        break;
-    }
+  case IMAGE_TYPE_NONE:
+    break;
+  }
 
-    return 0;
+  return 0;
 }
 
 bool image_transfer_iterate(void *data, enum image_type_enum type) {
 
-    switch (type) {
-    case IMAGE_TYPE_PNG:
+  switch (type) {
+  case IMAGE_TYPE_PNG:
 #ifdef HAVE_RPNG
-        if (!rpng_iterate_image((rpng_t *)data))
-            return false;
+    if (!rpng_iterate_image((rpng_t *)data))
+      return false;
 #endif
-        break;
-    case IMAGE_TYPE_JPEG:
+    break;
+  case IMAGE_TYPE_JPEG:
 #ifdef HAVE_RJPEG
-        return false;
+    return false;
 #else
-        break;
+    break;
 #endif
-    case IMAGE_TYPE_TGA:
+  case IMAGE_TYPE_TGA:
 #ifdef HAVE_RTGA
-        return false;
+    return false;
 #else
-        break;
+    break;
 #endif
-    case IMAGE_TYPE_BMP:
-        return false;
-    case IMAGE_TYPE_NONE:
-        return false;
-    }
+  case IMAGE_TYPE_BMP:
+    return false;
+  case IMAGE_TYPE_NONE:
+    return false;
+  }
 
-    return true;
+  return true;
 }
